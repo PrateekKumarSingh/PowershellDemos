d28583c461373c84162e1dae061728922fff5418
Prateek Kumar Singh <Prateek Kumar Singh>
2017-06-15 20:03:04 +0530

    update

--- Pipelines/Example_Speed.ps1
+++ Pipelines/Example_Speed.ps1
@@ -1,13 +1,3 @@
-# Without Pipeline
-$service = Get-Service
-foreach($item in $service){
-    ($item.DisplayName -replace " ","-").toUpper()
-    Start-Sleep -Seconds 1
-}
-
-# With Pipeline
-Get-Service | ForEach-Object {($_.DisplayName -replace " ","-").toUpper()}
-
 # Measuring the Speed
 (Measure-Command {  $service = Get-Service
                     foreach($item in $service){
